4bf08b9a8e5a40458c0e118f6ba6ae73590bab7e
Himanshu Chand Thakuri <np03cs4a220333@heraldcollege.edu.np>
2023-02-11 21:05:41 +0200

    my Portfolio

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+  function toggleDescription() {
+    const description = document.getElementById("description");
+    if (description.style.display === "none") {
+      description.style.display = "block";
+    } else {
+      description.style.display = "none";
+    }
+  }
+</script>
